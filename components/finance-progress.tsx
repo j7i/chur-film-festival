d92bfc306408c9158f0c6c9c31813fce98a515c3
Joel Willi <joel.willi@digitecgalaxus.ch>
2024-10-06 18:05:10 +0200

    improve mobile finance progress visu

--- components/finance-progress.tsx
+++ components/finance-progress.tsx
@@ -17,40 +17,51 @@ export const FinanceProgress: FunctionComponent<FinanceProgressProps> = ({
   const financedPercentage = (financed / total) * 100;
   const restructuringPercentage = (restructuring / total) * 100;
   return (
-    <div className={cn(className, "h-[160px] md:h-unset pr-[150px] md:pr-6")}>
+    <div className={cn(className, "h-[160px] md:h-unset pr-[120px] md:pr-6")}>
       <div className="relative w-full">
-      <div
-        className="absolute top-12 left-0 transform -translate-x-1/2"
-        style={{ left: `${financedPercentage}%` }}
-      >
-        <Badge className="border-dark-red" variant="outline">{formatCurrency(financed)}</Badge>
-      </div>
-      <div
-        className="absolute top-0 z-20 bottom-12 h-11 border-dark-red border-l-2"
-        style={{ left: `${financedPercentage}%` }}
-      />
-
-      <div
-        className="absolute top-12 left-0 transform -translate-x-1/2"
-        style={{ left: `${restructuringPercentage}%` }}
-      >
-        <Badge className="border-artsy-orange" variant="outline">{formatCurrency(restructuring)}</Badge>
-        <div className="absolute mt-2 text-sm md:text-lg">
-          Durchführung mit Umstrukturierung möglich
+        <div
+          className="absolute top-12 left-0 transform -translate-x-1/2"
+          style={{ left: `${financedPercentage}%` }}
+        >
+          <Badge className="border-dark-red" variant="outline">
+            {formatCurrency(financed)}
+          </Badge>
         </div>
-      </div>
-      <div
-        className="absolute top-0 z-20 bottom-12 h-11 border-dashed border-foreground border-l-2"
-        style={{ left: `${restructuringPercentage}%` }}
-      />
+        <div
+          className="absolute top-0 z-20 bottom-12 h-11 border-dark-red border-l-2"
+          style={{ left: `${financedPercentage}%` }}
+        />
 
-      <div className="absolute -top-[2px] md:top-12 -right-10 md:right-0 transform translate-x-1/2 z-10">
-        <Badge className="h-6 md:h-unset" variant="default">{formatCurrency(total)}</Badge>
-        <div className="absolute mt-[2px] md:mt-2 w-[220px] md:w-auto text-sm md:text-lg">Durchführung gesichert</div>
-      </div>
+        <div
+          className="absolute top-[80px] md:top-12 left-0 transform -translate-x-1/2"
+          style={{ left: `${restructuringPercentage}%` }}
+        >
+          <Badge className="border-artsy-orange" variant="outline">
+            {formatCurrency(restructuring)}
+          </Badge>
+          <div className="absolute mt-2 text-sm md:text-lg">
+            Durchführung mit Umstrukturierung möglich
+          </div>
+        </div>
+        <div
+          className="absolute top-0 z-20 bottom-12 h-[76px] md:h-11 border-dashed border-foreground border-l-2"
+          style={{ left: `${restructuringPercentage}%` }}
+        />
 
-      <Progress className="h-6" value={financedPercentage} max={total} />
-    </div>
+        <div className="absolute -top-[2px] md:top-12 -right-[24px] md:right-0 transform translate-x-1/2 z-10">
+          <Badge
+            className="h-6 md:h-unset rounded-l-none md:rounded-l-full"
+            variant="default"
+          >
+            {formatCurrency(total)}
+          </Badge>
+          <div className="absolute mt-[2px] md:mt-2 w-[220px] md:w-auto text-sm md:text-lg">
+            Durchführung gesichert
+          </div>
+        </div>
+
+        <Progress className="h-6" value={financedPercentage} max={total} />
+      </div>
     </div>
   );
 };
